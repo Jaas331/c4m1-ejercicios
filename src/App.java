2ce786be97c8e79a1f934cc9f43c38d8c70c9333
Johan Abello <jabellonjaas@gmail.com>
2025-07-06 22:10:56 -0500

    primera parte del ejercicio 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var input = new Scanner(System.in);
+        var salir = false;
+
+        do{
+            System.out.print("""
+                    .: Ejercicios del Modulo 1 :.
+                    =============================
+                    1.Verificar numero negativo
+
+                    0. salir
+
+                    Elija una opcion: """);
+            var opcion = input.nextInt();
+            input.nextLine();       
+            switch (opcion) {
+                case 1:
+                    verificarNumero(input);
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta!");
+                    break;
+            }
+
+        } while (!salir);
+        System.out.println("Hasta pronto!");
+        input.close();
+    }
+
+    /*
+    * Escriba un programa que permita ingresar un número real e identifique si el
+    * número es negativo, positivo o cero.
+    */
+    private static void verificarNumero(Scanner input) {
+        System.out.print("Por favor, ingrese el numero real a evaluar: ");
+        var real = input.nextDouble();
+
+        if (real == 0) {
+            System.out.println("El numero es cero.");   
+        } else if (real < 0) {
+            System.out.println("El numero es negativo.");
+        } else {
+            System.out.println("El numero es positivo.");
+        }
+
     }
 }
